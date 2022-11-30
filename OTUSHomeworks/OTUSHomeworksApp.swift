356c3b2ee523d985a6b6fc462f9c75319b8859e9
malakhoviv <malakhoviv@psbank.ru>
2022-11-30 22:25:08 +0300

    Initial Commit

---
+++ OTUSHomeworks/OTUSHomeworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTUSHomeworksApp.swift
+//  OTUSHomeworks
+//
+//  Created by Илья Малахов on 30.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct OTUSHomeworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
