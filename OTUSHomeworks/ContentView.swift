2e2bb439f85fa6bbb052514aa57fe79a2cc68f07
malakhoviv <malakhoviv@psbank.ru>
2022-12-19 22:54:42 +0300

    Сделал horizontal scroll view

--- OTUSHomeworks/ContentView.swift
+++ OTUSHomeworks/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            ListViewScreen<Species>()
+        GeometryReader { g in
+            ScrollView(.horizontal) {
+                HStack {
+                    ListViewScreen<People>()
+                        .frame(width: g.size.width - 5, height: g.size.height - 50, alignment: .center)
+                    ListViewScreen<Films>()
+                        .frame(width: g.size.width - 5, height: g.size.height - 50, alignment: .center)
+                    ListViewScreen<Starships>()
+                        .frame(width: g.size.width - 5, height: g.size.height - 50, alignment: .center)
+                    ListViewScreen<Vehicles>()
+                        .frame(width: g.size.width - 5, height: g.size.height - 50, alignment: .center)
+                    ListViewScreen<Species>()
+                        .frame(width: g.size.width - 5, height: g.size.height - 50, alignment: .center)
+                    ListViewScreen<Planets>()
+                        .frame(width: g.size.width - 5, height: g.size.height - 50, alignment: .center)
+                }
+            }
         }
     }
 }
